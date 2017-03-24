28eb49875407d91fb8c87c5a4fa500985312661c
Maxime Blais <maxime.j.blais@usherbrooke.ca>
2017-03-23 22:44:40 -0400

    Affichage de la grille terminé

---
+++ src/jeuechec/GrilleEchec.java
@@ -0,0 +1,24 @@
+package jeuechec;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import jeuechec.BoutonJeu;
+
+public class GrilleEchec extends JPanel {
+	static int nbLC = 8; // grandeur du ChessBoard
+	static int size = 75; // size d'une case
+	
+	public GrilleEchec(){
+		super(new GridLayout(nbLC, nbLC));
+		this.setPreferredSize(new Dimension(nbLC * size, nbLC * size));
+		
+		for (int i = 0; i < nbLC * nbLC; i++) {
+			this.add(new BoutonJeu(i));
+		}
+	}
+}
--- src/jeuechec/JeuEchecGUI.java
+++ src/jeuechec/JeuEchecGUI.java
@@ -1,10 +1,16 @@
 package jeuechec;
 
+import jeuechec.GrilleEchec;
+import javax.swing.JFrame;
+
 public class JeuEchecGUI {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		JFrame f = new JFrame();
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.add(new GrilleEchec());
+		f.pack();
+		f.setVisible(true);
 	}
 
 }
