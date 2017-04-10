c8ca1100463aea8ff29c6e5c968438c2fb05b3a2
JulieAnneLandry <julieannelandry94@gmail.com>
2017-04-10 12:54:49 -0400

    Add files via upload
    On a ajouté une fonction de test pour chaque pièce. Pour le moment, il y a seulement le Rook et le Bishop qui est vérifié comme il faut.

--- tests/TestOptionPiece.java
+++ tests/TestOptionPiece.java
@@ -20,6 +20,11 @@ public class TestOptionPiece
     private ArrayList<Piece> testList;
     private ArrayList<Point> verifPoint;
     private Piece rookTest;
+    private Piece bishopTest;
+    private Piece kingTest;
+    private Piece knightTest;
+    private Piece pawnTest;
+    private Piece queenTest;
     
     @Before   
     public void testOptionPiece(){
@@ -31,6 +36,233 @@ public class TestOptionPiece
     public void testOptionPieceAfter(){
         b = null;
         testList = null;
+    }
+    
+    @Test
+    public void testQueen()
+    {
+        testList = b.getAllPieces();
+        ArrayList<Piece> testList2 = new ArrayList<Piece>();
+        
+        for(Piece P : testList){
+            if(P instanceof Queen){
+                P.setOptions();
+                testList2.add(P);
+            }
+        }
+        
+        testList = testList2;
+        
+        for (int i=0;i<8;++i){
+        	if ((i+3) < 8)
+        		verifPoint.add(new Point(i,i+3));
+        	if ((7-i) >= 0)
+        		verifPoint.add(new Point(7-i,i+3));
+        	
+        	verifPoint.add(new Point(i,3));
+            verifPoint.add(new Point(0,i));
+        }
+           
+        for(Piece P:testList){
+            if(P.getCase().getPoint().x==0 && P.getCase().getPoint().y==3){
+                queenTest =P;
+                
+                break;
+            }
+        }
+        boolean testResult=true;
+        for(Point p :verifPoint){
+            if(!queenTest.getOptions().contains(p)){
+                testResult=false;
+                break;
+            }
+        }
+        
+        for(Point p :queenTest.getOptions()){
+            if(!verifPoint.contains(p)){
+                testResult=false;
+                break;
+            }
+        }
+        assertTrue(testResult);
+    }
+    
+    @Test
+    public void testPawn()
+    {
+        testList = b.getAllPieces();
+        ArrayList<Piece> testList2 = new ArrayList<Piece>();
+        
+        for(Piece P : testList){
+            if(P instanceof Pawn){
+                P.setOptions();
+                testList2.add(P);
+            }
+        }
+        
+        testList = testList2;
+        
+        for (int i=0;i<8;++i){
+            verifPoint.add(new Point(i,i+2));
+            verifPoint.add(new Point(7-i,i+2));
+        }
+           
+        for(Piece P:testList){
+            if(P.getCase().getPoint().x==1 && P.getCase().getPoint().y==0){
+                pawnTest =P;
+                
+                break;
+            }
+        }
+        boolean testResult=true;
+        for(Point p :verifPoint){
+            if(!pawnTest.getOptions().contains(p)){
+                testResult=false;
+                break;
+            }
+        }
+        
+        for(Point p :pawnTest.getOptions()){
+            if(!verifPoint.contains(p)){
+                testResult=false;
+                break;
+            }
+        }
+        assertTrue(testResult);
+    }
+    
+    @Test
+    public void testKnight()
+    {
+        testList = b.getAllPieces();
+        ArrayList<Piece> testList2 = new ArrayList<Piece>();
+        
+        for(Piece P : testList){
+            if(P instanceof Knight){
+                P.setOptions();
+                testList2.add(P);
+            }
+        }
+        
+        testList = testList2;
+        
+        for (int i=0;i<8;++i){
+            verifPoint.add(new Point(i,i+2));
+            verifPoint.add(new Point(7-i,i+2));
+        }
+           
+        for(Piece P:testList){
+            if(P.getCase().getPoint().x==0 && P.getCase().getPoint().y==1){
+                knightTest =P;
+                
+                break;
+            }
+        }
+        boolean testResult=true;
+        for(Point p :verifPoint){
+            if(!knightTest.getOptions().contains(p)){
+                testResult=false;
+                break;
+            }
+        }
+        
+        for(Point p :knightTest.getOptions()){
+            if(!verifPoint.contains(p)){
+                testResult=false;
+                break;
+            }
+        }
+        assertTrue(testResult);
+    }
+    
+    @Test
+    public void testKing()
+    {
+        testList = b.getAllPieces();
+        ArrayList<Piece> testList2 = new ArrayList<Piece>();
+        
+        for(Piece P : testList){
+            if(P instanceof King){
+                P.setOptions();
+                testList2.add(P);
+            }
+        }
+        
+        testList = testList2;
+        
+        for (int i=0;i<8;++i){
+            verifPoint.add(new Point(i,0));
+            verifPoint.add(new Point(0,i));
+        }
+           
+        for(Piece P:testList){
+            if(P.getCase().getPoint().x==0 && P.getCase().getPoint().y==4){
+                kingTest =P;
+                
+                break;
+            }
+        }
+        boolean testResult=true;
+        for(Point p :verifPoint){
+            if(!kingTest.getOptions().contains(p)){
+                testResult=false;
+                break;
+            }
+        }
+        
+        for(Point p :kingTest.getOptions()){
+            if(!verifPoint.contains(p)){
+                testResult=false;
+                break;
+            }
+        }
+        assertTrue(testResult);
+    }
+    
+    @Test
+    public void testBishop()
+    {
+        testList = b.getAllPieces();
+        ArrayList<Piece> testList2 = new ArrayList<Piece>();
+        
+        for(Piece P : testList){
+            if(P instanceof Bishop){
+                P.setOptions();
+                testList2.add(P);
+            }
+        }
+        
+        testList = testList2;
+        
+        for (int i=0;i<8;++i){
+        	if ((i+2) < 8)
+        		verifPoint.add(new Point(i,i+2));
+        	if ((7-i) >= 0)
+        		verifPoint.add(new Point(7-i,i+2));
+        }
+           
+        for(Piece P:testList){
+            if(P.getCase().getPoint().x==0 && P.getCase().getPoint().y==2){
+                bishopTest =P;
+                
+                break;
+            }
+        }
+        boolean testResult=true;
+        for(Point p :verifPoint){
+            if(!bishopTest.getOptions().contains(p)){
+                testResult=false;
+                break;
+            }
+        }
+        
+        for(Point p :bishopTest.getOptions()){
+            if(!verifPoint.contains(p)){
+                testResult=false;
+                break;
+            }
+        }
+        assertTrue(testResult);
     }
     
     
