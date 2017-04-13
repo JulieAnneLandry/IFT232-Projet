e99c7976d931a0bed870795d3600cc539384413c
Julie-Anne Landry <julieannelandry94@gmail.com>
2017-04-13 00:50:40 -0400

    Maudit merge qui est mieux d'avoir marché

--- src/chess/PlayMain.java
+++ src/chess/PlayMain.java
@@ -2,17 +2,29 @@ package chess;
 
 
 
+import java.util.ArrayList;
+
 import javax.swing.JFrame;
 
+import pieces.Piece;
 
 public class PlayMain {
 
 	public static void main(String[] args) {
 
+		Load l = new LoadFromFile();
+		ArrayList<Piece> pieces = l.load();
+		
+		Load l2 = new LoadFromPosition();
+		ArrayList<Piece> pieces2 = l2.load();
+		/*
+		 * Enlever le paramètre pour loader le board de départ
+		 * (pieces est une liste de pièces provenant du fichier saveFile.txt)
+		 */
 		Board grille = new Board();
 		grille.afficherTout();
-		Save s=new SavetoFile();
-		s.save(grille);
+		/*Save s = new SaveToFile();
+		s.save(grille);*/
 
 		
 		
