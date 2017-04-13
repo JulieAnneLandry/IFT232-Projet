7182fd0fca0d0df8e7a51790352981552d0a1a88
Julie-Anne Landry <julieannelandry94@gmail.com>
2017-04-13 00:57:10 -0400

    On revient à la case départ ENCORE.

--- src/chess/Board.java
+++ src/chess/Board.java
@@ -32,63 +32,6 @@ public class Board extends JPanel {
 		initPieces();
 	}
 
-	public Board(ArrayList<Piece> pieces) {
-		setSize(800, 700);
-		setLayout(new GridLayout(8, 8));
-		cases = new Case[8][8];
-		createCases();
-		pieceBox = pieces;
-		//initPieces();
-		
-		initFromFile(pieceBox);
-		
-		//initFromPosition(pieceBox);
-	}
-	
-	/*
-	 * CECI EST DU GARBAGE. À REFAIRE.
-	 * Ne pas supprimer. Il s'agit d'une leçon importante à retenir.
-	 * Maxime et Julie-Anne
-	 * xxx
-	 */
-	/*private void initFromPosition(ArrayList<Piece> pieces)
-	{
-		ArrayList<Case> casesBox = new ArrayList<Case>();
-		for (Piece p : pieces)
-		{
-			casesBox.add(p.getCase());
-		}
-		
-		int k = 0;
-		
-		for (int i = 0; i < 8; i++) {
-			for (int j = 0; j < 8; j++) {
-				
-				Case c = new Case(i, j);
-				if (!casesBox.contains(c))
-				{
-					casesBox.add(c);
-				}
-				add(casesBox.get(k));
-				cases[i][j] = casesBox.get(k);
-				
-				++k;
-			}
-		}
-	}*/
-
-	private void initFromFile(ArrayList<Piece> pieces) {
-		
-		for (Piece p : pieces)
-		{
-			addExistingPieces(p, (int)p.getCase().getPoint().getX(), (int)p.getCase().getPoint().getY());
-		}
-	}
-
-	private void addExistingPieces(Piece p, int x, int y) {
-		cases[x][y].setPiece(p);
-	}
-
 	public void initPieces() {
 
 		initTeam(true, 0, 1);
@@ -118,19 +61,17 @@ public class Board extends JPanel {
 		addPiece(new Bishop(team), row, 2);
 		addPiece(new Bishop(team), row, 5);
 
-		addPiece(new Queen(team), row, 3);
+		addPiece(new Queen(team), row, 4);
 
-		addPiece(new King(team), row, 4);
-		
-		
+		addPiece(new King(team), row, 3);
 	}
 
 	private void createCases() {
 		for (int i = 0; i < 8; i++) {
 			for (int j = 0; j < 8; j++) {
-				Case c = new Case(i, j);
-				add(c);
-				cases[i][j] = c;
+				Case p = new Case(j,i);
+				add(p);
+				cases[i][j] = p;
 			}
 		}
 	}
@@ -138,7 +79,6 @@ public class Board extends JPanel {
 	public void afficherTout() {
 		paintAll(getGraphics());
 	}
-	
 	public ArrayList<Piece> getAllPieces(){
 	   return pieceBox;
 	}
