eb79ac0e6a542c59941924b61ef272fb2ae62da1
Julie-Anne Landry <julieannelandry94@gmail.com>
2017-04-13 00:38:15 -0400

    Revert "Un autre essai de merge"
    This reverts commit 3cc250f8f9b39bb11f779c86268fec01bdae97f8.

--- src/chess/MovingPiece.java
+++ src/chess/MovingPiece.java
@@ -2,33 +2,45 @@ package chess;
 
 import pieces.Piece;
 
-// Sera encore sujet � modifications
+// Sera encore sujet � modifications
 
 public class MovingPiece {
 
-	private static Piece MOVING;
-	private static Case ORIGIN;
+    private static Piece MOVING;
+    private static Case ORIGIN;
+    private static boolean DEBUG = false; // True = déplacements libres, False = deplacements specifiques
 
-	public static void take(Piece p, Case c) {
-		if (p != null) {
-			MOVING = p;
-			ORIGIN = c;
-		}
-	}
+    public static void take(Piece p, Case c) {
+        if (p != null) {
+            MOVING = p;
+            ORIGIN = c;
+        }
+    }
 
-	public static void place(Case c) {
-		ORIGIN.removePiece();
-		c.setPiece(MOVING);
-		MOVING = null;
-		
-	}
+    public static void place(Case c) {
+        if (MOVING.isValidMove(c) && DEBUG == false){
+            ORIGIN.removePiece();
+            MOVING.firstMove();
+            c.setPiece(MOVING);
+        }
+        else if (DEBUG == true){
+            ORIGIN.removePiece();
+            MOVING.firstMove();
+            c.setPiece(MOVING);
+        }
+        else {
+            P.p("Ceci n'est pas un déplacement valide");
+        }
+        MOVING = null;
 
-	public static boolean isEmpty() {
-		if (MOVING == null) {
-			return true;
-		} else {
-			return false;
-		}
-	}
+    }
+
+    public static boolean isEmpty() {
+        if (MOVING == null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
 }
